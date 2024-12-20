2bb1a677d578ef90733db3a333d1de5cd49121f6
liurui <liurui_wuhan@163.com>
2024-12-20 11:39:47 +0800

    init

---
+++ docker/copy-html.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# 复制项目的文件到对应docker路径，便于一键生成镜像。
+usage() {
+	echo "Usage: sh copy-html.sh"
+	exit 1
+}
+
+# copy html
+echo "begin copy html to ack "
+cp -r ../dist ./ack/
+
+find ./ack/dist -type f -name "index.html" |xargs sed -i "s#datakit_env#prod#g"
+find ./ack/dist -type f -name "index.html" |xargs sed -i "s#datakit_service#mall-app-ack#g"
+echo 'ls ./ack'
+ls ./ack
+
+echo "begin copy html to vke "
+cp -r ../dist ./vke/
+find ./vke/dist -type f -name "index.html" |xargs sed -i "s#datakit_env#test#g"
+find ./vke/dist -type f -name "index.html" |xargs sed -i "s#datakit_service#mall-app-vke#g"
+echo 'ls ./vke'
+ls ./vke
